bbf23f2c3a26d4d4de1e71a199eaf9e47e335da5
Dice <102275981+Dice15@users.noreply.github.com>
2024-02-17 23:51:34 +0900

    Update: 이미지가 출력 안되는 현상 수정 테스트

--- src/app/home/desktop/guide/page.tsx
+++ src/app/home/desktop/guide/page.tsx
@@ -1,24 +1,30 @@
 import styles from "./page.module.css";
 import Image from 'next/image';
-import guide01 from '/public/guide01.PNG';
-import guide02 from '/public/guide02.PNG';
-import guide03 from '/public/guide03.PNG';
-import guide04 from '/public/guide04.PNG';
-import guide05 from '/public/guide05.PNG';
-import guide06 from '/public/guide06.PNG';
-import guide07 from '/public/guide07.PNG';
 
-export default async function DesktopGuide() {
-    // Render
+export default function DesktopGuide() {
     return (
         <div className={styles.desktopGuide}>
-            <Image src={guide01} alt="guide01" style={{ width: "80vw", height: "45vw", marginBottom: "50px" }} />
-            <Image src={guide02} alt="guide02" style={{ width: "80vw", height: "45vw", marginBottom: "50px" }} />
-            <Image src={guide03} alt="guide03" style={{ width: "80vw", height: "45vw", marginBottom: "50px" }} />
-            <Image src={guide04} alt="guide04" style={{ width: "80vw", height: "45vw", marginBottom: "50px" }} />
-            <Image src={guide05} alt="guide05" style={{ width: "80vw", height: "45vw", marginBottom: "50px" }} />
-            <Image src={guide06} alt="guide06" style={{ width: "80vw", height: "45vw", marginBottom: "50px" }} />
-            <Image src={guide07} alt="guide07" style={{ width: "80vw", height: "45vw", marginBottom: "50px" }} />
+            <div style={{ position: 'relative', width: "80vw", height: "45vw", marginBottom: "50px" }}>
+                <Image src="/guide01.PNG" alt="guide01" fill priority />
+            </div>
+            <div style={{ position: 'relative', width: "80vw", height: "45vw", marginBottom: "50px" }}>
+                <Image src="/guide02.PNG" alt="guide02" fill priority />
+            </div>
+            <div style={{ position: 'relative', width: "80vw", height: "45vw", marginBottom: "50px" }}>
+                <Image src="/guide03.PNG" alt="guide03" fill priority />
+            </div>
+            <div style={{ position: 'relative', width: "80vw", height: "45vw", marginBottom: "50px" }}>
+                <Image src="/guide04.PNG" alt="guide04" fill priority />
+            </div>
+            <div style={{ position: 'relative', width: "80vw", height: "45vw", marginBottom: "50px" }}>
+                <Image src="/guide05.PNG" alt="guide05" fill priority />
+            </div>
+            <div style={{ position: 'relative', width: "80vw", height: "45vw", marginBottom: "50px" }}>
+                <Image src="/guide06.PNG" alt="guide06" fill priority />
+            </div>
+            <div style={{ position: 'relative', width: "80vw", height: "45vw", marginBottom: "50px" }}>
+                <Image src="/guide07.PNG" alt="guide07" fill priority />
+            </div>
         </div>
     );
 }
