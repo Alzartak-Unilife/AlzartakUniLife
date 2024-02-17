8d1737ca14abae01dfbaae0cdb080cfa0bfa83b7
Dice <102275981+Dice15@users.noreply.github.com>
2024-02-18 00:22:17 +0900

    Update: 가이드 이미지 크기 수정

--- src/app/home/desktop/guide/page.tsx
+++ src/app/home/desktop/guide/page.tsx
@@ -4,25 +4,25 @@ import Image from 'next/image';
 export default function DesktopGuide() {
     return (
         <div className={styles.desktopGuide}>
-            <div style={{ position: 'relative', width: "80vw", height: "45vw", marginBottom: "50px" }}>
+            <div style={{ position: 'relative', width: "128vh", height: "72vh", marginBottom: "50px" }}>
                 <Image src="/guide01.PNG" alt="guide01" fill priority />
             </div>
-            <div style={{ position: 'relative', width: "80vw", height: "45vw", marginBottom: "50px" }}>
+            <div style={{ position: 'relative', width: "128vh", height: "72vh", marginBottom: "50px" }}>
                 <Image src="/guide02.PNG" alt="guide02" fill priority />
             </div>
-            <div style={{ position: 'relative', width: "80vw", height: "45vw", marginBottom: "50px" }}>
+            <div style={{ position: 'relative', width: "128vh", height: "72vh", marginBottom: "50px" }}>
                 <Image src="/guide03.PNG" alt="guide03" fill priority />
             </div>
-            <div style={{ position: 'relative', width: "80vw", height: "45vw", marginBottom: "50px" }}>
+            <div style={{ position: 'relative', width: "128vh", height: "72vh", marginBottom: "50px" }}>
                 <Image src="/guide04.PNG" alt="guide04" fill priority />
             </div>
-            <div style={{ position: 'relative', width: "80vw", height: "45vw", marginBottom: "50px" }}>
+            <div style={{ position: 'relative', width: "128vh", height: "72vh", marginBottom: "50px" }}>
                 <Image src="/guide05.PNG" alt="guide05" fill priority />
             </div>
-            <div style={{ position: 'relative', width: "80vw", height: "45vw", marginBottom: "50px" }}>
+            <div style={{ position: 'relative', width: "128vh", height: "72vh", marginBottom: "50px" }}>
                 <Image src="/guide06.PNG" alt="guide06" fill priority />
             </div>
-            <div style={{ position: 'relative', width: "80vw", height: "45vw", marginBottom: "50px" }}>
+            <div style={{ position: 'relative', width: "128vh", height: "72vh", marginBottom: "50px" }}>
                 <Image src="/guide07.PNG" alt="guide07" fill priority />
             </div>
         </div>
