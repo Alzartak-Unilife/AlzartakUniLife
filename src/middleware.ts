1a106ca9264842ac7bdd6399644f571098719e2d
Dice <102275981+Dice15@users.noreply.github.com>
2024-02-18 13:05:24 +0900

    Update: 미들웨어 주석 수정

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,17 +1,10 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { getToken } from 'next-auth/jwt';
 
-
 /**
- * Next.js 애플리케이션에 대한 미들웨어 로직입니다.
- * 
- * 요청된 경로에 따라 적절한 처리를 수행합니다.
- * 정적 파일이나 내부 요청, API 요청에 대해서는 다음 단계로 진행하고,
- * 로그인이 필요한 페이지에 대해서는 인증 여부를 확인합니다.
- * 또한, 사용자의 디바이스 타입과 페이지 타입이 일치하지 않으면 리다이렉트합니다.
- * 
- * @param {NextRequest} request - Next.js의 요청 객체입니다.
- * @returns {NextResponse} 처리 결과에 따른 응답 객체를 반환합니다.
+ * HTTP 요청에 대한 처리
+ * @param {NextRequest} request - 들어오는 요청 정보
+ * @returns {NextResponse} 요청에 따른 응답을 반환
  */
 export async function middleware(request: NextRequest) {
     // 정적 파일이나 내부 요청인 경우
@@ -25,112 +18,65 @@ export async function middleware(request: NextRequest) {
 
         // 특정 경로에 대한 처리
         switch (request.nextUrl.pathname) {
-            case "": {
+            case "":
+            case "/":
                 return caseRootUrl(request);
-            }
-            case "/": {
-                return caseRootUrl(request);
-            }
         }
     }
     return NextResponse.next();
 }
 
-
-
-
 /**
- * 주어진 요청이 Next.js의 정적 파일이나 내부 요청인지 판단합니다.
- * 
- * '/_next/'로 시작하는 경로와 '/favicon.ico' 경로는
- * Next.js의 정적 파일 또는 내부 요청으로 간주됩니다.
- * 이러한 경로에 대한 요청인 경우 true를 반환합니다.
- *
- * @param {NextRequest} request - 요청 객체입니다.
- * @returns {boolean} 정적 파일이나 내부 요청인 경우 true, 그렇지 않으면 false를 반환합니다.
+ * 요청이 정적 파일이나 내부 요청인지 판단
+ * @param {NextRequest} request - 요청 객체
+ * @returns {boolean} 정적 파일이나 내부 요청인 경우 true를 반환
  */
 function isInternalOrStaticRequest(request: NextRequest): boolean {
     const pathname = request.nextUrl.pathname;
-    if (pathname.startsWith('/_next/')) return true;
-    if (pathname.startsWith('/favicon.ico')) return true;
-    return false;
+    return pathname.startsWith('/_next/') || pathname.startsWith('/favicon.ico');
 }
 
-
-
-
 /**
- * 주어진 요청이 API 경로에 해당하는지 여부를 판단합니다.
- *
- * '/api'로 시작하는 경로는 API 요청으로 간주됩니다.
- * 이러한 경로에 대한 요청인 경우 true를 반환합니다.
- *
- * @param {NextRequest} request - 요청 객체입니다.
- * @returns {boolean} API 요청인 경우 true, 그렇지 않으면 false를 반환합니다.
+ * 요청이 API 요청인지 판단
+ * @param {NextRequest} request - 요청 객체
+ * @returns {boolean} API 요청인 경우 true를 반환
  */
 function isAPIRequest(request: NextRequest): boolean {
-    const pathname = request.nextUrl.pathname;
-    return pathname.startsWith('/api');
+    return request.nextUrl.pathname.startsWith('/api');
 }
 
-
-
-
 /**
- * 주어진 경로가 로그인(계정 인증)을 필요로 하는지 판별합니다.
- *
- * @param {NextRequest} request - 요청 객체입니다.
- * @returns {boolean} 로그인이 필요한 페이지인 경우 true, 그렇지 않으면 false를 반환합니다.
+ * 요청된 페이지가 인증을 필요로 하는지 판별
+ * @param {NextRequest} request - 요청 객체
+ * @returns {boolean} 인증이 필요한 페이지인 경우 true를 반환
  */
 function isRequireAuthentication(request: NextRequest): boolean {
-    const pathname = request.nextUrl.pathname;
-    if (pathname.startsWith("/timetable")) return true;
-    return false;
+    return request.nextUrl.pathname.startsWith("/timetable");
 }
 
-
-
-
 /**
- * 현재 요청의 호스트 URL을 구성합니다.
- *
- * @param {NextRequest} request - 요청 객체, 현재 요청의 URL 정보를 담고 있습니다.
- * @returns {string} - 현재 요청의 호스트 URL을 문자열로 반환합니다.
+ * 요청의 호스트 URL을 반환합니다.
+ * @param {NextRequest} request - 요청 객체입니다.
+ * @returns {string} 요청의 호스트 URL을 문자열로 반환
  */
 function getHostUrl(request: NextRequest): string {
     return `${request.nextUrl.protocol}//${request.nextUrl.host}`;
 }
 
-
-
-
 /**
- * 주어진 요청에 대해 사용자가 인증되었는지 여부를 판별합니다.
- *
- * 이 함수는 NextAuth의 `getToken` 함수를 사용하여
- * 요청에 포함된 세션 토큰을 검증하고, 유효한 세션이 있는지 확인합니다.
- * 유효한 세션이 있으면 true를, 그렇지 않으면 false를 반환합니다.
- *
- * @param {NextRequest} request - 검사할 요청 객체입니다.
- * @returns {Promise<boolean>} 사용자가 인증되었는지 여부를 나타내는 Promise 객체입니다.
+ * 요청에 포함된 사용자가 인증되었는지 여부를 확인
+ * @param {NextRequest} request - 요청 객체
+ * @returns {Promise<boolean>} 인증된 사용자인 경우 true를 반환
  */
 async function isUserAuthenticated(request: NextRequest): Promise<boolean> {
     const session = await getToken({ req: request, secret: process.env.ALZARTAK_NEXTAUTH_SECRET });
     return session !== null;
 }
 
-
-
-
 /**
- * 요청된 페이지의 타입과 사용자의 디바이스 타입이 일치하는지 판별합니다.
- *
- * 사용자 에이전트 문자열을 분석하여 모바일 디바이스인지 확인하고,
- * 요청된 URL의 경로가 'mobile'을 포함하는지 검사합니다.
- * 두 조건이 일치할 경우 true를, 그렇지 않으면 false를 반환합니다.
- *
- * @param {NextRequest} request - 검사할 요청 객체입니다.
- * @returns {boolean} 디바이스 타입과 페이지 타입이 일치하면 true, 그렇지 않으면 false를 반환합니다.
+ * 요청된 페이지와 사용자의 디바이스 타입이 일치하는지 판별 
+ * @param {NextRequest} request - 요청 객체
+ * @returns {boolean} 디바이스 타입과 페이지 타입이 일치하면 true를 반환
  */
 function isDeviceTypeMatchedWithPage(request: NextRequest): boolean {
     const userAgent = request.headers.get('user-agent') || '';
@@ -139,25 +85,16 @@ function isDeviceTypeMatchedWithPage(request: NextRequest): boolean {
     return isMobile === isMobilePage;
 }
 
-
-
-
 /**
- * 루트 URL('/')에 대한 요청을 처리하는 함수입니다.
- * 사용자의 장치 유형에 따라 적절한 페이지로 리다이렉트합니다.
- *
- * @param {NextRequest} request - 요청 객체, 사용자 에이전트 정보를 포함합니다.
- * @returns {NextResponse} - 리다이렉트에 대한 응답 객체를 반환합니다.
+ * 루트 URL('/')에 대한 요청을 처리
+ * @param {NextRequest} request - 요청 객체
+ * @returns {NextResponse} 사용자의 디바이스에 맞는 페이지로 리다이렉트
  */
 function caseRootUrl(request: NextRequest): NextResponse {
-    // 사용자의 device가 모바일인지 데스크톱인지 판별
     const userAgent = request.headers.get('user-agent') || '';
-    const isMobile = false;///mobile/i.test(userAgent);
-
-    // 리다이렉트 Url
+    const isMobile = false; // /mobile/i.test(userAgent)를 사용하여 모바일 디바이스 여부를 판단
     const redirectUrl = isMobile
         ? `${getHostUrl(request)}/home/mobile`
         : `${getHostUrl(request)}/home/desktop`;
-
     return NextResponse.redirect(new URL(redirectUrl));
 }
